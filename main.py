eaff9877e9e7f726bb60b5699c923384b7b7e014
Santje777 <s.v.oosterom@gmail.com>
2024-07-12 16:23:06 +0200

    Say hello three times from three users

---
+++ main.py
@@ -0,0 +1,11 @@
+class User:
+  """Defines a User class"""
+
+  def __init__(self):
+    """Initializes a new user"""
+    print("Hello from the User class")
+
+
+matt = User()
+emilie =  User()
+maria = User()
